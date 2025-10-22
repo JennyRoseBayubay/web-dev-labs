805b54a0275111e02cc132fee749e6693902c68b
Jenny Bayubay <jennyrosebayubay01@gmail.com>
2025-10-22 20:38:11 +0800

    add code

--- lab11/fix.js
+++ lab11/fix.js
@@ -1,14 +1,3 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <title>Error Correction</title>
-  <!--  If you save the corrected code as fix.js -->
-  <!-- <script src="fix.js"></script> -->
-</head>
-<body>
-  <!-- If you save the corrected code as fix.html -->
-  <div class="text-align-center p5 mb-4 bg-red">
-    <p>This box has multiple utility issues.</p>
-  </div>
-</body>
-</html>
+<div class="text-align-center p5 mb-4 bg-red">
+  <p>This box has multiple utility issues.</p>
+</div>
